56ba066e921f66ec1f740d597e4f32590c91ab57
Maya Murad <mayamon@MAYA.local>
2020-11-09 22:15:13 -0500

    Updates

---
+++ precache-manifest.0e2d16a74b970f3768b509664ed287b3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7178254295ddd05c7f267115536603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7428b896cd4d635ad182",
+    "url": "/static/css/2.67f619b1.chunk.css"
+  },
+  {
+    "revision": "86a353e866e7cd4cc532",
+    "url": "/static/css/main.25ef1143.chunk.css"
+  },
+  {
+    "revision": "7428b896cd4d635ad182",
+    "url": "/static/js/2.b3ff7812.chunk.js"
+  },
+  {
+    "revision": "de2b8e4407aa27f47c5f97de45c8eb4d",
+    "url": "/static/js/2.b3ff7812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a353e866e7cd4cc532",
+    "url": "/static/js/main.cc0b6e5e.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "237afd524cc5d8478d304b7599612ac9",
+    "url": "/static/media/LSP.237afd52.png"
+  },
+  {
+    "revision": "d0cf644bd3b7899be80e37e098930a09",
+    "url": "/static/media/Princess_Bubblegum.d0cf644b.png"
+  },
+  {
+    "revision": "117b42974c5cad1ce2a0f62a477bb5c7",
+    "url": "/static/media/Zelda.117b4297.png"
+  },
+  {
+    "revision": "3409ec6b8509e73aa48da4e203c3cfa5",
+    "url": "/static/media/avatar2.3409ec6b.png"
+  },
+  {
+    "revision": "c0168f11d79772f0e1baf90a62151746",
+    "url": "/static/media/colourshift.c0168f11.png"
+  },
+  {
+    "revision": "0c0fb19d3c2ce48f840b37a900952692",
+    "url": "/static/media/gameidentifier.0c0fb19d.png"
+  },
+  {
+    "revision": "ec9890cf45459493ec9f3604ff766bfd",
+    "url": "/static/media/maze.ec9890cf.png"
+  },
+  {
+    "revision": "c9e5510fffb5d1017597fd3194d33c57",
+    "url": "/static/media/resume.c9e5510f.pdf"
+  }
+]);
